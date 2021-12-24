6f99e23fbac9a95285f753f4eeca9c8672d5ed55
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-12-24 21:49:15 +0200

    import  routes in index.js file

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 dotenv.config();
 
 mongoose.connect(
@@ -21,7 +22,8 @@ mongoose.connect(
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
-
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 app.listen(8000, () => {
   console.log("Backend server is running");
 });
