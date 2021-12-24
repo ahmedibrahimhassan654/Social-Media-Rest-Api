db8d9b8afbd35623bbfe50a87c8ed77cd286d187
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-12-24 21:38:17 +0200

    connect db and add midelware to index.js

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+dotenv.config();
+
+mongoose.connect(
+  `process.env.MONGO_URL`,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
 app.listen(8000, () => {
   console.log("Backend server is running");
 });
